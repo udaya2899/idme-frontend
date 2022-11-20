4748e71e7d3c22096b8d71141b23600d77e28564
Udaya Prakash <udayaprakash2899@gmail.com>
2022-11-20 07:21:37 +0100

    fixed yarn lock, removed package lock, added verify code

--- src/app/pages/VerifyPage/index.tsx
+++ src/app/pages/VerifyPage/index.tsx
@@ -1,11 +1,33 @@
 import { Box } from '@mui/joy';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TabsBottomNav from '../../components/TabsBottomNav';
 import { QrReader } from 'react-qr-reader';
 
 export default function VerifyPage() {
   const [data, setData] = useState('No result');
 
+  async function verifyCreds(data) {
+    console.log('qr scanned');
+    console.log({ data });
+    const response = await fetch(
+      'http://131.159.209.212:8088/idme/verifier/verify/v1/vp',
+      {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        method: 'post',
+        body: JSON.stringify(data),
+      },
+    );
+
+    const responseData = await response.json();
+    console.log(responseData);
+  }
+
+  useEffect(() => {
+    verifyCreds(data);
+  }, [data]);
   return (
     <Box>
       <QrReader
@@ -20,7 +42,6 @@ export default function VerifyPage() {
         }}
         style={{ width: '100%' }}
       />
-      <p>{data}</p>
       <TabsBottomNav />
     </Box>
   );
